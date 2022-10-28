4f92c03b403808eaf851edcc9791e5e1bf17db32
O-sulloc <bujjaf@gmail.com>
2022-10-28 09:10:56 +0900

    boot

---
+++ src/test/java/com/example/bootexercise/BootExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
